60d66c83c53dc318a9514a6ab22818848db4626d
Carlos Loaiza <carloaiza@umanizales.edu.co>
2022-05-11 17:34:30 -0500

    Se agregan  métodos lista de

--- src/main/java/co/edu/umanizales/battleship/model/ListDE.java
+++ src/main/java/co/edu/umanizales/battleship/model/ListDE.java
@@ -13,16 +13,49 @@ public class ListDE {
     private int size;
 
     public void add(ShipDistribution shipDistribution) {
-
+        shipDistribution.getShip().setId(this.size+1);
+        if(this.head == null)
+        {
+            this.head= new NodeDE(shipDistribution);
+        }
+        else{
+            NodeDE temp = this.head;
+            while(temp.getNext() != null)
+            {
+                temp = temp.getNext();
+            }
+            //Posicionado en el último
+            temp.setNext(new NodeDE(shipDistribution));
+            temp.getNext().setPrevious(temp);
+        }
+        this.size++;
     }
 
     public void addToStart(ShipDistribution shipDistribution) {
-
+        shipDistribution.getShip().setId(this.size+1);
+        if(this.head == null)
+        {
+            this.head= new NodeDE(shipDistribution);
+        }
+        else
+        {
+            NodeDE newNode= new NodeDE(shipDistribution);
+            newNode.setNext(this.head);
+            this.head.setPrevious(newNode);
+            this.head = newNode;
+        }
+        this.size++;
     }
 
-    public List<ShipDistribution> list(){
+    public List<ShipDistribution> listNodes(){
         List<ShipDistribution> list= new ArrayList<>();
         //Recorre la listade y llenar la List
+        NodeDE temp= this.head;
+        while(temp != null)
+        {
+            list.add(temp.getData());
+            temp = temp.getNext();
+        }
         return list;
     }
 
--- src/main/java/co/edu/umanizales/battleship/service/ListDEService.java
+++ src/main/java/co/edu/umanizales/battleship/service/ListDEService.java
@@ -1,2 +1,30 @@
-package co.edu.umanizales.battleship.service;public class ListDEService {
+package co.edu.umanizales.battleship.service;
+
+import co.edu.umanizales.battleship.model.ListDE;
+import co.edu.umanizales.battleship.model.ShipDistribution;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ListDEService {
+    private ListDE listDE;
+
+    public ListDEService() {
+        listDE = new ListDE();
+    }
+
+    public String add(ShipDistribution shipDistribution){
+        listDE.add(shipDistribution);
+        return "Adicionado exitosamente";
+    }
+
+    public String addToStart(ShipDistribution shipDistribution){
+        listDE.addToStart(shipDistribution);
+        return "Adicionado exitosamente";
+    }
+
+    public List<ShipDistribution> listNodes(){
+        return listDE.listNodes();
+    }
 }
